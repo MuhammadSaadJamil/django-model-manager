55ab60ff7ba98b8fe21aedfa2c94d3a1c6ae29bb
Dev <saad.jamil@devsinc.com>
2022-07-06 12:12:33 +0500

    Added custom admin action

--- main/admin.py
+++ main/admin.py
@@ -2,12 +2,20 @@ from django.contrib import admin
 from django.contrib.admin import AdminSite
 from django.db import models
 from django import forms
+
 from .forms import ObjectForm
 from .models import Object
 from .adminFilters import NameFilter
 
 
-# @admin.register(Object)
+@admin.action(description="Update availability")
+def update_availability(modeladmin, request, queryset):
+    for data in queryset:
+        data.is_available = not data.is_available
+        data.save()
+
+
+@admin.register(Object)
 class ObjectAdmin(admin.ModelAdmin):
     list_display = ["name", "quantity", "is_available", "created_on", "display_date"]
     list_display_links = ["name", "quantity", "is_available", "created_on", "display_date"]
@@ -22,6 +30,7 @@ class ObjectAdmin(admin.ModelAdmin):
     list_filter = ['is_available', NameFilter, ('display_date', admin.EmptyFieldListFilter)]
     ordering = ('name', '-quantity',)
     search_fields = ['name', 'ingredients__name']
+    actions = [update_availability]
     fieldsets = (
         (None, {
             'fields': ("name", "description", "quantity", "is_available")
@@ -39,9 +48,9 @@ class ObjectAdmin(admin.ModelAdmin):
     }
 
 # My Custom Admin
-class MyAdminSite(AdminSite):
-    site_header = 'My Custom administration'
-
-
-admin_site = MyAdminSite(name='myadmin')
-admin_site.register(Object, ObjectAdmin)
+# class MyAdminSite(AdminSite):
+#     site_header = 'My Custom administration'
+#
+#
+# admin_site = MyAdminSite(name='myadmin')
+# admin_site.register(Object, ObjectAdmin)
